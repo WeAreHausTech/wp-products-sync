489df4b88b66eff65f5e5127508bdb52254b8c3e
Johanna Bertils <johanna@haus.se>
2025-05-14 13:23:30 +0200

    fix: enhance slug mismatch notice to display total mismatched posts count and show 50 instead of 5 at a time

--- src/Admin/Notices/SlugMismatch.php
+++ src/Admin/Notices/SlugMismatch.php
@@ -14,12 +14,43 @@ class SlugMismatch
             return;
         }
 
-        $postList = !empty($mismatchedPosts) ? '<span style="margin-left: 20px;">Products<span/><ul style="margin-left: 20px;">' . implode('', $mismatchedPosts) . '</ul>' : '';
+        $amountOfMismatchedPosts = self::getAmountOfMismatchedSlugPost();
+
+        $postList = '';
+        if (!empty($mismatchedPosts)) {
+            $postLabel = 'Products (' . count($mismatchedPosts);
+            if ($amountOfMismatchedPosts > count($mismatchedPosts)) {
+                $postLabel .= ' of ' . $amountOfMismatchedPosts;
+            }
+            $postLabel .= ')';
+            $postList = '<span style="margin-left: 20px;">' . $postLabel . '</span><ul style="margin-left: 20px;">' . implode('', $mismatchedPosts) . '</ul>';
+        }
         $termList = !empty($mismatchedTerms) ? '<span style="margin-left: 20px;">Taxonomies<span/><ul style="margin-left: 20px;">' . implode('', $mismatchedTerms) . '</ul>' : '';
 
         $allList = $postList . $termList;
         $message = "Products/taxonomies detected where the slug does not match between WordPress and Vendure. This may cause issues with links or product visibility. Please check the following items:";
         AdminSettingsUI::renderAdminNotice('error', 'Slug Mismatch Detected', $message, $allList, false);
+    }
+
+
+    private static function getAmountOfMismatchedSlugPost()
+    {
+        $args = [
+            'post_type' => 'produkter',
+            'meta_query' => [
+                [
+                    'key' => 'vendure_slug_mismatch',
+                    'value' => '1',
+                    'compare' => '=',
+                ],
+            ],
+            'fields' => 'ids',
+            'posts_per_page' => -1
+        ];
+
+        $mismatchedPosts = get_posts($args);
+        $totalCount = count($mismatchedPosts);
+        return $totalCount;
     }
 
     private static function getMismatchedSlugPosts()
@@ -34,9 +65,14 @@ class SlugMismatch
                 ],
             ],
             'fields' => 'ids',
+            'posts_per_page' => 50
         ];
 
         $mismatchedPosts = get_posts($args);
+        $countArgs = $args;
+        $countArgs['posts_per_page'] = -1;
+
+        $totalCount = count(get_posts($countArgs));
 
         if (empty($mismatchedPosts)) {
             return [];
